690b6ae8c6b2f02aa0a88d75cbcb664b8bd53979
darnoldi <davearnoldi@Daves-iMac.local>
2014-08-29 18:11:58 +0200

    Initial Commit

---
+++ Out Of This World/DAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DAAppDelegate.h
+//  Out Of This World
+//
+//  Created by Dave Arnoldi on 2014/08/29.
+//  Copyright (c) 2014 Dave Arnoldi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out Of This World/DAViewController.h
@@ -0,0 +1,13 @@
+//
+//  DAViewController.h
+//  Out Of This World
+//
+//  Created by Dave Arnoldi on 2014/08/29.
+//  Copyright (c) 2014 Dave Arnoldi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAViewController : UIViewController
+
+@end
